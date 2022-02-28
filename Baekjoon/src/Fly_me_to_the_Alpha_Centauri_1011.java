4774c42eec7807135ff74ee26c42012d7ede3f66
HyeBin, Park <35947674+HYEBPARK@users.noreply.github.com>
2022-03-01 00:14:02 +0900

    2/28

--- Baekjoon/src/Fly_me_to_the_Alpha_Centauri_1011.java
+++ Baekjoon/src/Fly_me_to_the_Alpha_Centauri_1011.java
@@ -8,25 +8,14 @@ public class Flyme_1011 {
         for (int i = 0; i < T; i++) {
             long start = sc.nextLong();
             long end = sc.nextLong();
-            long cnt = 0, cur = 0;
             long distance = end - start;
-
-            while (distance != 0 && distance > (end - start) /2) {
-                if(distance - (cur + 1) >= (end - start) / 2)
-                {
-                    distance = distance - (cur + 1);
-                    cur = cur + 1;
-                }
-                else
-                    break;
-                cnt++;
-            }
-            if(distance > (end - start) /2)
-                cnt += cnt +1;
-            else if((end-start)/2 >= 1 && distance <= (end - start) /2)
-                cnt += cnt;
-
-            sb.append(cnt);
+            long max = (long)Math.sqrt(distance);
+            if (max == Math.sqrt(distance))
+                sb.append((max * 2) - 1);
+            else if(distance <= max * max + max)
+                sb.append(max * 2);
+            else
+                sb.append((max * 2) + 1);
             if(i < T-1)
                 sb.append("\n");
         }
